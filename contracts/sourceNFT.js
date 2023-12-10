2bd65f504eb137461f28c37c6c9372d0c300cb87
dchaebae <danielkchae@gmail.com>
2023-12-09 22:10:47 -0500

    sourceNFT updated

--- contracts/sourceNFT.js
+++ contracts/sourceNFT.js
@@ -24,13 +24,7 @@ console.log(apiResponse)
 if (apiResponse.error) {
   throw new Error("Response Error");
 }
-
 // fetch the price
 const val = apiResponse.data;
-
 console.log(val);
 return Functions.encodeString(val)
-// price * 100 to move by 2 decimals (Solidity doesn't support decimals)
-// Math.round() to round to the nearest integer
-// Functions.encodeUint256() helper function to encode the result from uint256 to bytes
-//return Functions.encodeUint256(Math.round(price * 100));
